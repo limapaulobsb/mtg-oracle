800025d8dacb2a769e44622e1be6bcc7034d0927
Paulo Henrique Lima <limapaulobsb@gmail.com>
2022-02-01 19:27:35 -0300

    Initial setup with React Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Oracle from './pages/Oracle';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <h1>MtG Oracle</h1>
       </header>
-    </div>
+      <Routes>
+        <Route path='/' element={ <Oracle /> } />
+        <Route path='*' element={ <NotFound /> } />
+      </Routes>
+    </main>  
   );
 }
 
---
+++ src/pages/Oracle.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Oracle() {
+  return <h1>Oracle</h1>;
+}
+
+export default Oracle;
